9938a875fb0fc22459737a54cef66d2df30b8c98
Igor Pardinho <pardinhoigor@gmail.com>
2025-07-28 09:43:30 -0300

    cria o componente Home

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Home from "./pages/home/Home";
+
+function App() {
+  return (
+    <>
+      <Home
+        titulo="Seja Bem Vinde"
+        texto="Expresse aqui seus pensamentos e opniões"
+      ></Home>
+    </>
+  );
+}
+
+export default App;
